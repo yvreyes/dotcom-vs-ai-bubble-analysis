61858b8304c22e8a99b912bde5b4df835a2a2bbe
Yuri Reyes <yurivreyes.dev@gmail.com>
2026-07-27 00:06:15 +0800

    Handle missing values

--- src/validate.py
+++ src/validate.py
@@ -2,12 +2,26 @@ import pandas as pd
 from config import RAW_DATA_PATH
 
 def main():
-    pass
+    validate_csv_files()
 
-for csv_file in RAW_DATA_PATH.glob("*.csv"):
-    df = pd.read_csv(csv_file)
-    print("===================================")
-    print(f"File: {csv_file.name}")
-    print(f"Rows: {len(df)}")
-    print(f"Columns: {len(df.columns)}")
-    print("===================================")
+def validate_csv_files():
+    for csv_file in RAW_DATA_PATH.glob("*.csv"):
+        df = pd.read_csv(csv_file)
+       
+
+        for col in df.columns:   
+
+            print("===================================")
+            print(f"File: {csv_file.name}\n")
+            print(f"Rows: {len(df)}")
+            print(f"Columns: {len(df.columns)}\n")   
+            print("Missing Values:")
+            print(f"Open: {df['Open'].isnull().sum()}")
+            print(f"High: {df['High'].isnull().sum()}")
+            print(f"Low: {df['Low'].isnull().sum()}")
+            print(f"Close: {df['Close'].isnull().sum()}")
+            print(f"Volume: {df['Volume'].isnull().sum()}")
+            print("===================================") 
+
+if __name__ == "__main__":
+    main()
