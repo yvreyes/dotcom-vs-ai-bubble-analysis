c06257b79a925f80f1c970ea755a0049fea9841f
Yuri Reyes <yurivreyes.dev@gmail.com>
2026-08-13 20:07:35 +0800

    Print output

--- src/validate.py
+++ src/validate.py
@@ -2,13 +2,29 @@ import pandas as pd
 from config import RAW_DATA_PATH
 
 def main():
-    missing_values()
-
-def missing_values():
     for csv_file in RAW_DATA_PATH.glob("*.csv"):
-        df = pd.read_csv(csv_file)
+        df = pd.read_csv(csv_file) 
 
-        for col in df.columns:
-            columns = f"{col}: {df[col].isnull().sum()}"
-            print(columns)
+        print("=============================")
+        print(f"File: {csv_file.name}")
+        print("=============================\n")
+
+
+        print(df.columns)
+        print(df.head())
+        # print(f"Rows: {len(df)}")
+        # print(f"Columns: {len(df.columns)}\n")
+
+        # missing_values(df)
+        # duplicate_values(df)
+
+def missing_values(df):
+    print("Missing Values:")
+    for col in df.columns:
+        print(f"{col}: {df[col].isnull().sum()}")    
+
+def duplicate_values(df):
+    duplicates = df.duplicated().sum()
+    print(f"Duplicate rows: {duplicates}\n")
+    
 main()
