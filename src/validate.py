dc82bbfd09dfcaf3630a25bf45c15128963d6316
Yuri Reyes <yurivreyes.dev@gmail.com>
2026-08-15 21:56:40 +0800

    validate date update

--- src/validate.py
+++ src/validate.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from config import RAW_DATA_PATH
 
+required_col = ["Date", "Open", "High", "Low", "Close", "Volume"]
 def main():
-    required_col = ["Date", "Open", "High", "Low", "Close", "Volume"]
     for csv_file in RAW_DATA_PATH.glob("*.csv"):
         df = pd.read_csv(csv_file) 
 
@@ -11,29 +11,32 @@ def main():
         print("=============================\n")
 
         
-        # for col_name in df.columns:
-        #     if col_name in required_col:
-        #         print(f"{col_name} exists")
-        #     elif col_name not in required_col:
-        #         print(f"{col_name} is missing")
-        #     else:
-        #         print(f"{col_name} doesnt exists")    
-
-        for col_name in required_col:
-            if col_name in df.columns:
-                print(f"{col_name} exists")
-            elif col_name not in df.columns:
-                print(f"{col_name} is missing")
-            else:
-                print(f"{col_name} doesnt exists")               
-
-        # print(df.head())
-       
+        validate_date(df)
+    
         # print(f"Rows: {len(df)}")
         # print(f"Columns: {len(df.columns)}\n")
 
         # missing_values(df)
         # duplicate_values(df)
+
+def validate_columns(df):
+    # Check if columns exist
+    for col_name in required_col:
+        if col_name in df.columns:
+            print(f"{col_name} exists")
+        else:
+            print(f"{col_name} doesnt exists")               
+
+def validate_date(df):
+    # print("Date Validation: \n")
+    
+    for find_date in df["Price"]:
+
+        if find_date == "Date":
+           print("Date marker found")
+
+    #date order chronological
+        
 
 def missing_values(df):
     print("Missing Values:")
